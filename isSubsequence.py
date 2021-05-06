3f2dfc43f3ea68defac5556619045df3996f54b5
Punit Khandelwal <60363697+punitkcse@users.noreply.github.com>
2021-05-06 15:11:17 +0530

    Create isSubsequence.py

---
+++ isSubsequence.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isSubsequence(self, sequence: str, array: str) -> bool:
+        arrIdx = 0
+        seqIdx = 0
+        while(arrIdx<len(array) and seqIdx<len(sequence)):
+            if(array[arrIdx]==sequence[seqIdx]):
+                seqIdx +=1
+            arrIdx +=1
+        return seqIdx == len(sequence)
